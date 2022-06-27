f3ad0298055cadd8819f1acfbfd23b5777765b56
Vu Viet Thang <vuvietthang1508@gmail.com>
2022-06-27 19:30:43 +0700

    thêm logic báo lỗi nếu có

--- Crop Image/CroppedImageViewController.swift
+++ Crop Image/CroppedImageViewController.swift
@@ -20,14 +20,26 @@ class CroppedImageViewController: UIViewController {
     }
     
     @objc func showAlert(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
-        let title = "Image saved"
-        let message = "Image has been saved to Library"
-        
-        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
-        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {_ in
-            self.dismiss(animated: true)
-        }))
-        self.present(alert, animated: true, completion: nil)
+        if error == nil{
+            let title = "Image saved"
+            let message = "Image has been saved to Library"
+            
+            let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {_ in
+                self.dismiss(animated: true)
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
+        else {
+            let title = "Error"
+            let message = "Image could not be saved. An error has occured!"
+            
+            let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {_ in
+                alert.dismiss(animated: true)
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
     }
 
     @IBAction private func saveImage(_ sender: UIButton) {
